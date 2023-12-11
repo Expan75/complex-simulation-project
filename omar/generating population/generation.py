cccd68a3734a3c67c1186cb5f81cec0e70c1b317
OmarBark <73150660+OmarBark@users.noreply.github.com>
2023-12-11 18:47:47 +0100

    added specification

--- omar/generating population/generation.py
+++ omar/generating population/generation.py
@@ -19,6 +19,21 @@ KD
 MP
 SD 
 """
+"""
+rows
+
+'lrecon','lrecon_sd','lrecon_salience'
+                   ,'lrecon_dissent','lrecon_blur','galtan','galtan_sd',
+                   'galtan_salience','galtan_dissent','galtan_blur','immigrate_policy',
+                   'immigrate_salience','immigrate_dissent','multiculturalism','multicult_salience',
+                   'multicult_dissent','redistribution','redist_salience','environment',
+                   'enviro_salience','spendvtax','deregulation','econ_interven',
+                   'civlib_laworder','sociallifestyle','religious_principles','ethnic_minorities'
+                   ,'nationalism','urban_rural','protectionism','regions',
+                   'russian_interference','anti_islam_rhetoric','people_vs_elite','antielite_salience',
+                   'corrupt_salience','members_vs_leadership'
+
+"""
 np.savetxt("pop.csv", population, delimiter=",", header='V, S, C,L,M,KD,MP,SD', comments='')
 
 
